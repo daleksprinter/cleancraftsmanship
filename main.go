86d3459f37426031bee8a9d2b93f76e354080025
r-sekine <ca01376@cartahd.com>
2023-12-21 17:04:40 +0900

    fix

--- main.go
+++ main.go
@@ -1,10 +1,19 @@
 package main
 
 type Stack struct {
+	empty bool
+}
+
+func NewStack() *Stack {
+	return &Stack{empty: true}
 }
 
 func (s *Stack) IsEmpty() bool {
-	return false
+	return s.empty
+}
+
+func (s *Stack) Push(i int) {
+	s.empty = false
 }
 
 func main() {
--- main_test.go
+++ main_test.go
@@ -3,9 +3,19 @@ package main
 import "testing"
 
 func TestCanCreateStack(t *testing.T) {
-	s := Stack{}
+	s := NewStack()
 	actual := s.IsEmpty()
 	expected := true
+	if actual != expected {
+		t.Errorf("got %v\nwant %v", actual, expected)
+	}
+}
+
+func TestAfterOnePushIsNotEmpty(t *testing.T) {
+	s := NewStack()
+	s.Push(0)
+	actual := s.IsEmpty()
+	expected := false
 	if actual != expected {
 		t.Errorf("got %v\nwant %v", actual, expected)
 	}
